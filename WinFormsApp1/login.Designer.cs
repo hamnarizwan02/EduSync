07186ea8d3c3201537bf59073b7c7f6695a89701
Kissa Zahra <141498732+kissasium@users.noreply.github.com>
2024-03-20 17:36:28 +0500

    login front-end done

--- WinFormsApp1/login.Designer.cs
+++ WinFormsApp1/login.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "login";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            panel1 = new Panel();
+            showFiles = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(242, 247, 253);
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.Location = new Point(-6, -18);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(428, 470);
+            panel1.TabIndex = 1;
+            // 
+            // showFiles
+            // 
+            showFiles.BackColor = Color.Black;
+            showFiles.FlatAppearance.BorderSize = 0;
+            showFiles.FlatStyle = FlatStyle.Flat;
+            showFiles.Font = new Font("Century Gothic", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            showFiles.ForeColor = Color.White;
+            showFiles.Location = new Point(483, 286);
+            showFiles.Name = "showFiles";
+            showFiles.Size = new Size(133, 33);
+            showFiles.TabIndex = 25;
+            showFiles.Text = "LOGIN";
+            showFiles.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(369, 167);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 22);
+            label1.TabIndex = 26;
+            label1.Text = "Email ✉";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(331, 198);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 22);
+            label2.TabIndex = 27;
+            label2.Text = "password 🔒";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(483, 162);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(168, 27);
+            textBox1.TabIndex = 28;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(483, 198);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(168, 27);
+            textBox2.TabIndex = 29;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(375, 18);
+            label3.Name = "label3";
+            label3.Size = new Size(238, 51);
+            label3.TabIndex = 30;
+            label3.Text = "WELCOME";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.MidnightBlue;
+            label4.Location = new Point(483, 237);
+            label4.Name = "label4";
+            label4.Size = new Size(118, 16);
+            label4.TabIndex = 31;
+            label4.Text = "forgot password?";
+            label4.Click += label4_Click;
+            // 
+            // login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(242, 247, 253);
+            ClientSize = new Size(762, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(showFiles);
+            Controls.Add(panel1);
+            Name = "login";
+            Text = "login";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Panel panel1;
+        private Button showFiles;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
     }
 }
--- WinFormsApp1/login.cs
+++ WinFormsApp1/login.cs
@@ -16,5 +16,15 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
